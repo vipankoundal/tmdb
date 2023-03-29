bcbda7584ac95d06abcb0d31c29ea4c5ec6ada62
Vipan <115575172+vipankoundal@users.noreply.github.com>
2023-03-29 07:59:32 +0530

    Delete netflix directory

--- netflix/src/config/config.js
+++
@@ -1,8 +0,0 @@
-//image sizes for tmdb
-export const img_300 = "https://image.tmdb.org/t/p/w300";
-export const img_500 = "https://image.tmdb.org/t/p/w500";
-export const original = "https://image.tmdb.org/t/p/original";
-
-// contentModal and singleContent
-export const unavailable =
-  "https://www.movienewz.com/img/films/poster-holder.jpg";
